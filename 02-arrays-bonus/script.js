7c0fb85841cb6bafa823c9598aa5b866b1362a8f
alessiodalberto002 <aledalberto002.com>
2025-12-09 20:35:48 +0100

    longnames completato

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,9 +15,11 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(name => name.length > 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
