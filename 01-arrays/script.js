36218a21910d6c56d54de8e219bc622974adbfe2
alessiodalberto002 <aledalberto002.com>
2025-12-10 18:55:42 +0100

    Aggiunta e rimozione di primo e ultimo insegnante

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,12 +18,17 @@ teachers[4]="Patrick";
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
