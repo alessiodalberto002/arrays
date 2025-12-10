8f50b9d021ddd4076862322702f00c42a17ebc24
alessiodalberto002 <aledalberto002.com>
2025-12-10 18:40:52 +0100

    aggiustamento codice dell'array al contrario

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,8 +10,12 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
-console.log(reversedTeachers);
+
+const revervedTeachers = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+    revervedTeachers.push(teachers[i]);
+  }
+console.log(revervedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
